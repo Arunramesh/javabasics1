2f6d67bc8c7b174361e475453ebba69012301e5f
Arunkumar <arunramesh1598@gmail.com>
2018-05-30 12:05:01 +0530

    Programs

---
+++ Demo1.java
@@ -0,0 +1,30 @@
+
+public class Demo1 {
+	public static void main(String s[]) {
+		byte one=0;
+		byte two=1;
+		float f=2.0f;
+		byte three=(byte)(one + two);
+		System.out.println(three);
+		System.out.println(f);
+		float fOne=f+two;
+		System.out.println(fOne);
+		int count=3;
+		count++;
+		System.out.println(count);
+		count--;
+		System.out.println(count);
+		System.out.println(one > two);
+		System.out.println(one <= two);
+		boolean answer=true;
+		System.out.println(!answer);
+		System.out.println(20 & 16);
+		System.out.println(20 | 16);
+		String name="Gajini";
+		int code=9065;
+		System.out.println("Welcome Mr."+name+" and your password is "+code+"");
+		
+		
+	}
+
+}
---
+++ Demo2.java
@@ -0,0 +1,20 @@
+
+
+	public class Demo2
+{
+public static void main( String[] args )
+{
+int i=0;
+while(i<10) {
+	System.out.println("X is"+i);
+	i++;
+}
+
+}
+}
+	
+
+	
+
+
+
